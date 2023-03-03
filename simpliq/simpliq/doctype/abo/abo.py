afe09f894e59d2526f0f8b871f93ebdc3d0246f7
Jonathan Furrer <azureuser@vmfrappe14.ujedcwrsvxjurlo3jakddsigje.zrhx.internal.cloudapp.net>
2023-03-03 16:33:18 +0000

    deletet libracore doctypes

--- simpliq/simpliq/doctype/abo/abo.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, simpliq and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Abo(Document):
-	pass
--- simpliq/simpliq/doctype/abo_item/abo_item.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, simpliq and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Abo Item', {
-	// refresh: function(frm) {
-
-	// }
-});
